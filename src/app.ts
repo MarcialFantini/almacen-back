ec0575eae058e1c3413a37de604655d8a856290f
MarcialFantini <marcialfantini@gmail.com>
2024-02-22 12:51:57 -0300

    initial state

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+// configures dotenv to work in your application
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app
+  .listen(PORT, () => {
+    console.log("Server running at PORT: ", PORT);
+  })
+  .on("error", (error) => {
+    // gracefully handle error
+    throw new Error(error.message);
+  });
